851c546356d454668cd7957dca9d7052307e9dad
Peter Tumulty <ptumulty923@gmail.com>
2022-02-12 06:31:57 -0500

    updated the image caching image sizes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   images: {
     domains: ['images.prismic.io', 'res.cloudinary'],
     minimumCacheTTL: 31536000,
-    deviceSizes: [639, 767, 1023, 1279, 1535],    
+    deviceSizes: [639, 767, 1023, 1279, 1535],
+    imageSizes: [320, 480, 640, 768, 924, 1180, 1436],   
   },
 };
