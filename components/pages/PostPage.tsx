26ff6f7c2b832ea6f3a9a50b502a844096cf7cfb
Peter Tumulty <ptumulty923@gmail.com>
2022-06-26 12:57:28 -0400

    started working on the blog post template

--- components/pages/PostPage.tsx
+++ components/pages/PostPage.tsx
@@ -3,6 +3,7 @@ import SiteHead from 'components/shared/SiteHead';
 import Container from 'layouts/Container';
 import FadeInContainer from 'layouts/FadeInContainer';
 import dynamic from 'next/dynamic';
+import Image from "next/image";
 import React from 'react';
 import {Post} from 'types/Post';
 import { IFeaturedReview } from 'types/Review';
@@ -28,7 +29,24 @@ const PostPage = ({ post, featuredReview }: Props) => {
       <main>
         <Container>
 
-         <p>We...</p>
+          <div className="flex flex-col w-full sm:flex-row">
+
+            <div className="w-full sm:w-3/4">
+              <h1 className="mb-12 text-3xl leading-loose text-gray-600 sm:text-5xl md:text-6xl">{post?.post_title[0]?.text}</h1>
+              {post?.featured_image && (
+                 <Image
+                   src={post?.featured_image?.url}
+                   alt={post?.post_title[0]?.text}
+                   width={post?.featured_image?.dimensions?.width}
+                   height={post?.featured_image?.dimensions?.height}
+                   layout="intrinsic"
+               />
+              )}
+            </div>
+            <div className="w-full sm:w-1/4">
+               Sidebar..
+            </div>
+          </div>
         </Container>
       </main>
       <FeaturedReview {...featuredReview} />
