00d9eeca03fe73e66e237ed62dd3bcf2561cda05
Peter Tumulty <ptumulty923@gmail.com>
2022-07-03 12:08:24 -0400

    finished first pass on blog post template

--- components/pages/PostPage.tsx
+++ components/pages/PostPage.tsx
@@ -11,6 +11,9 @@ import { IFeaturedReview } from 'types/Review';
 import { SEO_DEFAULTS, SITE_URL } from 'utils/constants';
 import { urlify } from 'utils/helpers';
 import { AiOutlineFacebook, AiOutlineInstagram } from 'react-icons/ai';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
+import { format } from 'date-fns';
+
 const FeaturedReview = dynamic(
   () => import('components/shared/FeaturedReview')
 );
@@ -33,9 +36,17 @@ const PostPage = ({ post, featuredReview }: Props) => {
         <Container>
           <div className="flex flex-col w-full sm:flex-row">
             <div className="w-full sm:w-2/3">
-              <h1 className="mb-12 text-3xl leading-loose text-gray-600 sm:text-5xl md:text-6xl">
+              <h1 className="mb-4 text-3xl text-gray-600 sm:leading-loose sm:text-5xl md:text-6xl">
                 {post?.post_title[0]?.text}
               </h1>
+              <div className="flex flex-row w-full mb-4 text-stone-600">
+                <p>
+                  <strong> Author: </strong> {post?.author?.name[0]?.text}
+                  <span className="mx-4">|</span>
+                  <strong>Publish date: </strong>
+                  {format(new Date(post?.publish_date), 'LLLL M, yyyy')}
+                </p>
+              </div>
               {post?.featured_image && (
                 <Image
                   src={post?.featured_image?.url}
@@ -45,6 +56,12 @@ const PostPage = ({ post, featuredReview }: Props) => {
                   layout="intrinsic"
                 />
               )}
+              <div className="blog-post-sub-title">
+                <RichText render={post?.sub_title} />
+              </div>
+              <div className="blog-post-content">
+                <RichText render={post?.post_content} />
+              </div>
             </div>
             <div className="w-full sm:w-1/3 sm:px-12">
               <div className="flex flex-col w-full pb-3 mb-4 border-b-2">
@@ -97,5 +114,4 @@ const PostPage = ({ post, featuredReview }: Props) => {
     </FadeInContainer>
   );
 };
-
 export default PostPage;
--- types/Post.ts
+++ types/Post.ts
@@ -1,26 +1,25 @@
-import {RichTextBlock} from "prismic-reactjs";
+import { RichTextBlock } from 'prismic-reactjs';
 
-interface Image  {
-    dimensions: {
-      width: string;
-      height: string;
-    }
-    alt: null;
-    copyright: null;
-    url: string;
-  }
+interface Image {
+  dimensions: {
+    width: string;
+    height: string;
+  };
+  alt: null;
+  copyright: null;
+  url: string;
+}
 
 export interface Post {
   sub_title: RichTextBlock[];
   featured_image: Image;
   post_title: RichTextBlock[];
   post_content: RichTextBlock[];
-  publish_data: string;
+  publish_date: string;
   seo_meta_title: string;
   seo_meta_description: string;
   author: {
     name: RichTextBlock[];
     profile_image: Image;
-  }
-
+  };
 }
