a590b65fcd6f8c5acd203afacb8d73b8fc8187f1
ptums <ptumulty923@gmail.com>
2023-03-12 21:35:35 -0400

    added pdf

--- components/pages/PostPage.tsx
+++ components/pages/PostPage.tsx
@@ -1,17 +1,16 @@
-import ProfileHeader from 'components/organisms/coach/ProfileHeader';
 import SiteHead from 'components/shared/SiteHead';
 import Container from 'layouts/Container';
 import FadeInContainer from 'layouts/FadeInContainer';
 import dynamic from 'next/dynamic';
 import Image from 'next/legacy/image';
 import Link from 'next/link';
-import React from 'react';
-import { SEO_DEFAULTS, SITE_URL } from 'utils/constants';
+import { SEO_DEFAULTS } from 'utils/constants';
 import { urlify } from 'utils/helpers';
 import { AiOutlineFacebook, AiOutlineInstagram } from 'react-icons/ai';
 import { format } from 'date-fns';
 import { IBlogPostFields, IReviewFields } from 'types/contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { useRouter } from 'next/router';
 
 const FeaturedReview = dynamic(
   () => import('components/shared/FeaturedReview')
@@ -24,6 +23,7 @@ interface Props {
 }
 
 const PostPage = ({ post, review, postContent }: Props) => {
+  const router = useRouter();
   return (
     <FadeInContainer>
       <SiteHead
@@ -58,40 +58,69 @@ const PostPage = ({ post, review, postContent }: Props) => {
                   </span>
                 </p>
               </div>
-              {post?.featuredImage && (
-                <Image
-                  src={post?.featuredImage?.url}
-                  alt={post?.postTItle as string}
-                  width={post?.featuredImage?.width}
-                  height={post?.featuredImage?.height}
-                  layout="intrinsic"
-                />
-              )}
+              {router.asPath.includes(
+                'work-with-victims-of-domestic-violence'
+              ) ? (
+                <>
+                  <div
+                    style={{
+                      position: 'relative',
+                      overflow: 'hidden',
+                      width: '100%',
+                      paddingTop: '150.25%',
+                    }}
+                  >
+                    <iframe
+                      style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        right: 0,
+                        width: '100%',
+                        height: '100%',
+                      }}
+                      src="/MyWorkwithVictimsofDomesticViolence.pdf#toolbar=0"
+                    ></iframe>
+                  </div>
+                </>
+              ) : (
+                <>
+                  {post?.featuredImage && (
+                    <Image
+                      src={post?.featuredImage?.url}
+                      alt={post?.postTItle as string}
+                      width={post?.featuredImage?.width}
+                      height={post?.featuredImage?.height}
+                      layout="intrinsic"
+                    />
+                  )}
 
-              <div className="blog-post-content">
-                {documentToReactComponents(postContent?.json)}
-              </div>
+                  <div className="blog-post-content">
+                    {documentToReactComponents(postContent?.json)}
+                  </div>
+                </>
+              )}
             </div>
             <div className="w-full sm:w-1/3 sm:px-12">
               <div className="flex flex-col w-full pb-3 mb-4 border-b-2">
                 <Link
                   href={`/coach/${urlify(post?.author?.name[0]?.text || '')}`}
+                  passHref
                 >
-                  <a>
-                    <div className="relative w-24 h-24">
-                      <Image
-                        src={post?.author?.profileImage?.url}
-                        alt={post?.author?.name}
-                        layout="fill"
-                        objectFit="cover"
-                        className="rounded-full"
-                      />
-                    </div>
-                    <p className="mt-3 text-gray-600">
-                      <strong className="block">Meet our coach</strong>{' '}
-                      {post?.author?.name}
-                    </p>
-                  </a>
+                  <div className="relative w-24 h-24">
+                    <Image
+                      src={post?.author?.profileImage?.url}
+                      alt={post?.author?.name}
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-full"
+                    />
+                  </div>
+                  <p className="mt-3 text-gray-600">
+                    <strong className="block">Meet our coach</strong>{' '}
+                    {post?.author?.name}
+                  </p>
                 </Link>
               </div>
               <div className="p-2 bg-gray-100 rounded">
