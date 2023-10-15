fccaec20136306f5ddc6714d078d9452b6375b9f
ptums <ptumulty923@gmail.com>
2023-10-14 21:10:40 -0400

    removed original constans from file

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,119 +6,10 @@ export const NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN:
 export const FAUNA_SECRET: string | undefined =
   process.env.NEXT_PUBLIC_FAUNA_SECRET;
 
-export const BOOLEAN_CHOICE = [
-  {
-    name: 'Yes',
-    checked: false,
-  },
-  {
-    name: 'No',
-    checked: true,
-  },
-];
-
-export const SERVICES_OPTIONS = [
-  {
-    name: 'Excellent',
-    checked: false,
-  },
-  {
-    name: 'Good',
-    checked: false,
-  },
-  {
-    name: 'Disappointing',
-    checked: false,
-  },
-];
-
-export const SITE_NAVS = [
-  {
-    id: 1,
-    slug: '/',
-    label: 'Home',
-  },
-  {
-    id: 2,
-    slug: null,
-    label: 'Our Team',
-    children: [
-      {
-        id: 1,
-        slug: '/coach/jessica-rebelo',
-        label: 'Jessica Rebelo',
-        open: false,
-      },
-      {
-        id: 3,
-        slug: 'https://themendwellness.com/',
-        label: 'Kristin Brien',
-        open: true,
-      },
-    ],
-  },
-  {
-    id: 3,
-    slug: '/services',
-    label: 'Services',
-  },
-  {
-    id: 5,
-    slug: null,
-    label: 'Media',
-    children: [
-      {
-        id: 1,
-        slug: '/blog',
-        label: 'Blog',
-      },
-      {
-        id: 2,
-        slug: '/interviews',
-        label: 'Interviews',
-      },
-      {
-        id: 3,
-        slug: '/podcast',
-        label: 'Podcast',
-      },
-    ],
-  },
-  {
-    id: 6,
-    slug: '/reviews',
-    label: 'Reviews',
-  },
-];
-
-export const SERVICES_LIST = [
-  {
-    id: 1,
-    slug: '/service/emdr',
-    service: 'EMDR',
-  },
-  {
-    id: 2,
-    slug: '/service/trauma-therapy',
-    service: 'Trauma Therapy',
-  },
-  {
-    id: 3,
-    slug: '/service/trauma-informed-life-coaching',
-    service: 'Trauma-Informed Life Coaching',
-  },
-];
-
 export const FAILED_EMAIL_MESSAGE: string =
   'There was an issue with your submission, please email the details to unleashedpotential@gmail.com';
 export const successEmailMessage = (page: string) =>
   `Thank you for your ${page} submission. We will reach out you shortly!`;
-
-export const SEO_DEFAULTS = {
-  title: 'Unleashed Potential Life Coaching',
-  metaDescription:
-    'Unleashed Potential: Life Coaching helps you uncover your purpose by identifying negative behaviors and giving a plan to achieve your goals.',
-};
 
 export const SITE_URL =
   process.env.NODE_ENV === 'production'
@@ -129,13 +20,6 @@ export const CONTACT_INFO = [
   'Jessica: (631) 432-8897',
   'Unleashedpotentiallifecoaching@gmail.com',
 ];
-
-export const SITE_TITLE = 'Unleashed Potential';
-
-export const BLOG_PAGE_ID = '6zowWUPrZmyZ0HjQXBnoye';
-export const PODCAST_PAGE_ID = '2TrjFekCNc6OGZAsLLB7WK';
-export const REVIEWS_PAGE_ID = '7M8IWPzBF60jrl0SiHgUsW';
-export const INTERVIEWS_PAGE_ID = '3o5X9MvYCjNeqDdHOQSFZd';
 
 export const headers = {
   'Content-Type': 'application/json',
