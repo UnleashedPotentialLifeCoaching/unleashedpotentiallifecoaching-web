21c9fc3b05ef2c26bac30be1feab694b8482dd06
ptums <ptumulty923@gmail.com>
2024-06-19 07:50:29 -0400

    fixed comma issue

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -33,7 +33,7 @@ export const getServerSideProps: GetServerSideProps = async (
 
     `;
     })
-    .join(',');
+    .join('');
 
   const siteLinks = constants?.site_navigation
     .map(
@@ -69,7 +69,7 @@ export const getServerSideProps: GetServerSideProps = async (
       `,
               ),
     )
-    .join(',');
+    .join('');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">      
