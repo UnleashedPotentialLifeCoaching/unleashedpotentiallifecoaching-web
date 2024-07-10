4666a8774c598dea8e28f64b3f78bf194c30ec63
ptums <ptumulty923@gmail.com>
2024-07-10 15:02:06 -0400

    started working on the cta button

--- components/atoms/AnimatedButton.tsx
+++ components/atoms/AnimatedButton.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const AnimatedButton = () => {
+const AnimatedButton = ({
+  children,
+}: {
+  children: React.ReactElement[] | React.ReactElement;
+}) => {
   return (
-    <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 block">
-      Get in touch!
-    </button>
+    <motion.div
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ['0%', '0%', '50%', '50%', '0%'],
+      }}
+      transition={{
+        duration: 2,
+        ease: 'easeInOut',
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+    >
+      <button className="px-4 py-4 bg-transparent text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 block">
+        <div className="border py-4 px-6">{children}</div>
+      </button>
+    </motion.div>
   );
 };
 
--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -5,61 +5,89 @@ import FeaturedMessage from 'components/organisms/home/FeaturedMessage';
 import WidgetWrapper from 'components/organisms/home/WidgetWrapper';
 import Coaches from 'components/organisms/home/Coaches';
 import Container from 'layouts/Container';
-import { ICoachFields, IHomePageFields, IReviewFields } from 'types/contentful';
+import { ICoachFields, IHomePageFields } from 'types/contentful';
+import Link from 'next/link';
+import { useMemo } from 'react';
+import CarouselCTA from 'components/shared/CarouselCTA';
+import AnimatedButton from 'components/atoms/AnimatedButton';
 
 interface Props {
   page: IHomePageFields;
   coaches: ICoachFields[];
 }
+let V1_COACHES = false;
 
-const HomePage = ({ page, coaches }: Props) => (
-  <FadeInContainer>
-    <SiteHead
-      title={page?.seoTitle}
-      metaDescription={page?.seoMetaDescription}
-    />
-    <main>
-      <HomeBanner
-        imageUrl={page?.banner?.url}
-        lineOne={page?.mainBannerText}
-        lineTwo={page?.subBannerText}
+const HomePage = ({ page, coaches }: Props) => {
+  const widgets = useMemo(
+    () => [
+      {
+        title: page?.widgetOneTitle,
+        imageUrl: page?.widgetOneImage?.url,
+        description: page?.widgetOneMessage,
+      },
+      {
+        title: page?.widgetTwoTitle,
+        imageUrl: page?.widgetTwoImage?.url,
+        description: page?.widgetTwoMessage,
+      },
+      {
+        title: page?.widgetThreeTitle,
+        imageUrl: page?.widgetThreeImage?.url,
+        description: page?.widgetThreeMessage,
+      },
+      {
+        title: page?.widgetFourTitle,
+        imageUrl: page?.widgetFourImage?.url,
+        description: page?.widgetFourMessage,
+      },
+    ],
+    [page],
+  );
+  return (
+    <FadeInContainer>
+      <SiteHead
+        title={page?.seoTitle}
+        metaDescription={page?.seoMetaDescription}
       />
-      <Container>
-        <FeaturedMessage
-          imageUrl={page?.featuredImage?.url}
-          header={page?.featuredMessageHeader}
-          body={page?.featuredMessageBody}
+      <main>
+        <HomeBanner
+          imageUrl={page?.banner?.url}
+          lineOne={page?.mainBannerText}
+          lineTwo={page?.subBannerText}
         />
-      </Container>
-      <Coaches coaches={coaches} />
-      <Container>
-        <WidgetWrapper
-          widgets={[
-            {
-              title: page?.widgetOneTitle,
-              imageUrl: page?.widgetOneImage?.url,
-              description: page?.widgetOneMessage,
-            },
-            {
-              title: page?.widgetTwoTitle,
-              imageUrl: page?.widgetTwoImage?.url,
-              description: page?.widgetTwoMessage,
-            },
-            {
-              title: page?.widgetThreeTitle,
-              imageUrl: page?.widgetThreeImage?.url,
-              description: page?.widgetThreeMessage,
-            },
-            {
-              title: page?.widgetFourTitle,
-              imageUrl: page?.widgetFourImage?.url,
-              description: page?.widgetFourMessage,
-            },
-          ]}
-        />
-      </Container>
-    </main>
-  </FadeInContainer>
-);
+        <Container>
+          <FeaturedMessage
+            imageUrl={page?.featuredImage?.url}
+            header={page?.featuredMessageHeader}
+            body={page?.featuredMessageBody}
+          />
+        </Container>
+        {V1_COACHES ? (
+          <Coaches coaches={coaches} />
+        ) : (
+          <>
+            <div className="relative z-10 flex justify-center inline-block inset-y-56">
+              <AnimatedButton>
+                <Link href="/coach/jessica-rebelo">
+                  <p className="text-white font-serif italic font-bold text-center text-3xl">
+                    Take the first step
+                  </p>
+                  <p className="font-bold mb-0 text-base text-center">
+                    Get your first 1 hour session today!
+                  </p>
+                </Link>
+              </AnimatedButton>
+            </div>
+            <CarouselCTA />
+          </>
+        )}
+
+        <Container>
+          <WidgetWrapper widgets={widgets} />
+        </Container>
+      </main>
+    </FadeInContainer>
+  );
+};
 
 export default HomePage;
